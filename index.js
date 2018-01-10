accf8e6d419f81dd8d145672941b04c59a8ec330
Annette Coll <annette.coll@outlook.com>
2018-01-10 11:30:50 -0500

    css changes + added sources

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const hbs = require('hbs')
 const parser = require('body-parser')
 const restaurants = require('./controllers/restaurants')
+
 //this library allows us to do push and put requests
 const methodOverride = require('method-override')
 
@@ -22,6 +23,53 @@ app.use(parser.urlencoded({ extended: true })) // handles form submissions
 
 app.get('/', (req, res) => {
     res.render('index')
+})
+
+// use mongoose to retrieve or display restaurants from database
+
+app.get('/', (request, respond) => {
+// return list of all restaurants
+  console.log('get' + Restaurants.find({}))
+  Restaurants.find({})
+      .then((restaurantsData) => {
+          respond.render('index',{
+              restaurants: restaurantsData
+          })
+      })
+      .catch((err) => {
+          console.log(err)
+      })
+})
+
+app.get('/:name', (request, respond) => {
+  let name = request.params.name
+  Restaurants.findOne({name: name})
+  .then(restaurants => {
+    respond.render('restaurants-show', { restaurants: restaurants})
+  })
+})
+
+//GL
+// for when the browser wants to send info back
+// to the server
+app.post( '/', (request, respond) => {
+  console.log('in post')
+  Restaurants.create(request.body.recipe)
+    .then( restaurants => {
+      // if the restaurants exists then go to the recipe page
+      respond.redirect(`/${restaurants.name}`)
+    })
+})
+
+
+//GL
+//update restaurants request
+app.put('/:name', (request, respond) => {
+  let name = request.params.name
+  Restaurants.findOneAndUpdate({name : name})
+    .then(restaurants => {
+      res.redirect(`/${restaurants.name}`)
+    })
 })
 
 
